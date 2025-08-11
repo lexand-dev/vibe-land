85ed5f3add294447383646c594374afa075504b7
alexander flores <alexfloresrayme@gmail.com>
2025-08-11 13:27:48 -0500

    feat(ui-home): implement layout and navbar components; add project form and projects list functionality

---
+++ src/modules/home/ui/components/navbar.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+
+export const Navbar = () => {
+  return (
+    <nav
+      className={cn(
+        "p-4 bg-transparent fixed top-0 left-0 right-0 z-50 transition-all duration-200 border-b border-transparent"
+      )}
+    >
+      <div className="max-w-5xl mx-auto w-full flex justify-between items-center">
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="Vibe" width={24} height={24} />
+          <span className="font-semibold text-lg">Vibe Land</span>
+        </Link>
+        <div>
+          <Button variant="default" size="sm">
+            Login
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
--- src/modules/projects/server/procedures.ts
+++ src/modules/projects/server/procedures.ts
@@ -4,8 +4,31 @@ import { prisma } from "@/lib/db";
 import { inngest } from "@/inngest/client";
 import { generateSlug } from "random-word-slugs";
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 
 export const projectsRouter = createTRPCRouter({
+  getOne: baseProcedure
+    .input(
+      z.object({
+        id: z.string().min(1, "Project ID is required")
+      })
+    )
+    .query(async ({ input }) => {
+      const message = await prisma.project.findUnique({
+        where: {
+          id: input.id
+        }
+      });
+
+      if (!message) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: `Project not found`
+        });
+      }
+
+      return message;
+    }),
   getMany: baseProcedure.query(async () => {
     const projects = await prisma.project.findMany({
       orderBy: {
